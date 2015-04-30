6ca0a4a829ba12a3d5ae35dd3b5e3249c46cd18b
sxend <arimitsu.k+github@gmail.com>
2015-04-30 16:24:24 +0900

    remove file

--- generate-conf.sh
+++ generate-conf.sh
@@ -2,8 +2,8 @@
 
 CONFD_DIR=usr/local/nginx/conf.d/
 CONFD_TEMPLATES=`find ${CONFD_DIR} -name '*.conf.template'`
-BIND_VARIABLES=\'`cat ./require_environment_variable | sed -e 's/\(.*\)/\$\{\1\}/' | tr '\n' ' '`\'
-
+BIND_VARIABLES="'"'${DOCKER_REGISTRY_HOST_NAME} ${ASF_SITE_HOST_NAME}'"'"
+echo $BIND_VARIABLES
 for TEMPLATE in $CONFD_TEMPLATES
 do
   GENERATED_FILE=$CONFD_DIR`basename $TEMPLATE .template`

f9b58bbfad4b642f13492e2f803174807d49ea7b
sxend <arimitsu.k+github@gmail.com>
2015-05-02 19:15:16 +0900

    avoid all download

--- bin/startup.sh
+++ bin/startup.sh
@@ -4,7 +4,7 @@ envsubst '${DOCKER_REGISTRY_HOST}' < /usr/local/nginx/conf.d/docker-registry.ngi
 
 service nginx start
 
-aws s3 sync s3://jenkins.arimit.su/root/.jenkins/ /root/.jenkins/
+aws s3 sync s3://jenkins.arimit.su/root/.jenkins/jobs/ /root/.jenkins/jobs/
 
 nohup java -jar /opt/jenkins.war --httpPort=7000 2>&1 > /root/.jenkins/jenkins.log &
 
